8e29c725f6f4a4459414354d599196c820995697
Matthew Paletta <Matthew@JOE.local>
2017-07-08 15:42:54 -0700

    removed hyphens in env variables

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ with open('config.json') as data_file:
 	data = json.load(data_file)
 
 # These vars are loaded in from config.
-consumer_key = os.environ["consumer-key"]
-consumer_secret = os.environ["consumer-secret"]
-access_token_key = os.environ["access-token-key"]
-access_token_secret = os.environ["access-token-secret"]
+consumer_key = os.environ["consumerkey"]
+consumer_secret = os.environ["consumersecret"]
+access_token_key = os.environ["accesstokenkey"]
+access_token_secret = os.environ["accesstokensecret"]
 retweet_update_time = data["retweet-update-time"]
 scan_update_time = data["scan-update-time"]
 rate_limit_update_time = data["rate-limit-update-time"]
