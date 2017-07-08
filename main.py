b419d30f00b976379a648dfbe04b3f46a5c10bdb
Matthew Paletta <Matthew@JOE.local>
2017-07-08 15:57:04 -0700

    Fixed names

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ with open('config.json') as data_file:
 
 # These vars are loaded in from config.
 consumer_key = os.environ["consumerkey"]
-consumer_secret = os.environ["consumersecret"]
+consumer_secret = os.environ["consumerkeysecret"]
 access_token_key = os.environ["accesstokenkey"]
 access_token_secret = os.environ["accesstokensecret"]
 retweet_update_time = data["retweet-update-time"]
